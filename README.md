5c183451701fb9b45af13848bf26b63f7e22e44a
zachchao <zach.chao@yahoo.com>
2018-11-19 19:34:06 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flask Skeleton
+A simple flask skeleton to be cloned and then used for starting small flask applications easily. The HTML and CSS isn't perfect but the skeleton will allow you to get started quickly. To use, go to the directory where you'd like to start and run, `git clone https://github.com/zachchao/FlaskSkeleton`.
---
+++ app.py
@@ -0,0 +1,19 @@
+import json
+from flask import Flask
+from flask import render_template, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
